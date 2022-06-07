427307e7bcf13e79ce850c604580f09fe14da177
YuretzOguretz <99295219+YuretzOguretz@users.noreply.github.com>
2022-06-07 15:24:51 -0400

    initial

---
+++ src/NumberSelector.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yungu3633
+ */
+public class NumberSelector {
+    private int num;
+    private static String row1 = "123456789";
+    private static String row2 = "123456789";
+    private static String row3 = "123456789";
+    private static String row4 = "123456789";
+    private static String row5 = "123456789";
+    private static String row6 = "123456789";
+    private static String row7 = "123456789";
+    private static String row8 = "123456789";
+    private static String row9 = "123456789";
+    private static String box1 = "123456789";
+    private static String box2 = "123456789";
+    private static String box3 = "123456789";
+    private static String box4 = "123456789";
+    private static String box5 = "123456789";
+    private static String box7 = "123456789";
+    private static String box8 = "123456789";
+    private static String box9 = "123456789";
+    private static String line1 = "123456789";
+    private static String line2 = "123456789";
+    private static String line3 = "123456789";
+    private static String line4 = "123456789";
+    private static String line5 = "123456789";
+    private static String line6 = "123456789";
+    private static String line7 = "123456789";
+    private static String line8 = "123456789";
+    private static String line9 = "123456789";
+    
+    public int NumberSelector(){
+        num = (int)(Math.random()*8)+1;
+        return num;
+    }
+}
