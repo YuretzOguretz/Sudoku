b22188192200cc64cdfca48082197e3ac0f07149
jiingg <99295219+YuretzOguretz@users.noreply.github.com>
2022-06-09 17:14:56 -0400

    Correction numberselector1

--- src/NumberSelector.java
+++ src/NumberSelector.java
@@ -11,95 +11,148 @@
 public class NumberSelector {
     private String num;
     private static String temp1 = "";
-    private static String row1 = "123456789";
-    private static String row2 = "123456789";
-    private static String row3 = "123456789";
-    private static String row4 = "123456789";
-    private static String row5 = "123456789";
-    private static String row6 = "123456789";
-    private static String row7 = "123456789";
-    private static String row8 = "123456789";
-    private static String row9 = "123456789";
-    private static String box1 = "123456789";
-    private static String box2 = "123456789";
-    private static String box3 = "123456789";
-    private static String box4 = "123456789";
-    private static String box5 = "123456789";
-    private static String box6 = "123456789";
-    private static String box7 = "123456789";
-    private static String box8 = "123456789";
-    private static String box9 = "123456789";
-    private static String line1 = "123456789";
-    private static String line2 = "123456789";
-    private static String line3 = "123456789";
-    private static String line4 = "123456789";
-    private static String line5 = "123456789";
-    private static String line6 = "123456789";
-    private static String line7 = "123456789";
-    private static String line8 = "123456789";
-    private static String line9 = "123456789";
+    private static String row [] = new String[9];
+    private static String box [] = new String[9];
+    private static String line [] = new String[9];
+    
+    public NumberSelector(){
+        for (int index = 0; index < 9; index++) {
+            row[index] = "123456789";
+            box[index] = "123456789";
+            line[index] = "123456789";
+            
+        }
+    }
     
     public String NumberSelector(int rowX, int rowY){
-//        num = (int)(Math.random()*8)+0; (Choosing random string)
-        boolean found = false;
-        if (rowX==1&&rowY==1){
-        num = ""+RandomNumber(row1);
-        while(found==false){
-            if ((row1.contains(num) && box1.contains(num) && line1.contains(num))){
-               RemoveNum(row1,num);
-               RemoveNum(box1,num);
-               RemoveNum(line1,num);
-               found = true;
-            }
-            else{
-               num = ""+RandomNumber(row1);
-            }
+        
+        
+
+        if ((rowX>= 1&&rowX<= 3)&&((rowY>= 1&&rowY<= 3))){
+          //Box #1
+          int section = 0;
+          num = UsebleRandomnum( rowX-1 ,  rowY-1 ,  section);
         }
+        else if ((rowX>= 1&&rowX<= 3)&&((rowY>= 4&&rowY<= 6))){
+          //Box #2  
+          int section = 1;
+          num = UsebleRandomnum( rowX-1 ,  rowY-1 ,  section);
         }
+        else if ((rowX>= 1&&rowX<= 3)&&((rowY>= 7&&rowY<= 9))){
+          //Box #3 
+          int section = 2;
+          num = UsebleRandomnum( rowX-1 ,  rowY-1 ,  section);
+        }
+        else if ((rowX>= 4&&rowX<= 6)&&((rowY>= 1&&rowY<= 3))){
+          //Box #4  
+          int section = 3;
+          num = UsebleRandomnum( rowX-1 ,  rowY-1 ,  section);
+        }
+        else if ((rowX>= 4&&rowX<= 6)&&((rowY>= 4&&rowY<= 6))){
+          //Box #5
+          int section = 4;
+          num = UsebleRandomnum( rowX-1 ,  rowY-1 ,  section);
+        }
+        else if ((rowX>= 4&&rowX<= 6)&&((rowY>= 7&&rowY<= 9))){
+          //Box #6  
+          int section = 5;
+          num = UsebleRandomnum( rowX-1 ,  rowY-1 ,  section);
+        }
+        else if ((rowX>= 7&&rowX<= 9)&&((rowY>= 1&&rowY<= 3))){
+          //Box #7 
+          int section = 6;
+          num = UsebleRandomnum( rowX-1 ,  rowY-1 ,  section);
+        }
+        else if ((rowX>= 7&&rowX<= 9)&&((rowY>= 4&&rowY<= 6))){
+          //Box #8 
+          int section = 7;
+          num = UsebleRandomnum( rowX-1 ,  rowY-1 ,  section);
+        }
+        else if ((rowX>= 7&&rowX<= 9)&&((rowY>= 7&&rowY<= 9))){
+          //Box #9  
+          int section = 8;
+          num = UsebleRandomnum( rowX-1 ,  rowY-1 ,  section);
+        }
+        else {
+            num = "k";
+        }
+
         return num;
     } 
+    
+    public String UsebleRandomnum(int a, int b, int c){
+        boolean found = false;
+        
+        num = ""+RandomNumber(row[a]);
+        while(found==false){
+            if (row[a].indexOf(num)==-1 && box[c].indexOf(num)==-1 && line[b].indexOf(num)==-1){
+               num = ""+RandomNumber(row[a]);
+            }
+            else{
+               RemoveNumFromRow(row[a],num ,a);
+               RemoveNumFromBox(box[c],num,c);
+               RemoveNumFromLine(line[b],num, b);
+               found = true;
+               
+               
+            }
+        }
+        found = false;
+        return num;
+    }
     
     public int RandomNumber(String n){
        int i = (int) (Math.random()*n.length()-1)+0;
        return i;
     }
     
-    public void RemoveNum(String box, String num){
+    public void RemoveNumFromRow(String box, String num1, int numA){
         for (int f =0; f<box.length(); f++){
-              if (!(box.substring( f, f+1).equals(num))){
+              if (!(box.substring( f, f+1).equals(num1))){
                    temp1+=box.substring( f, f+1);
+                   System.out.println(temp1);
               }
           }
-        box=temp1;
+        box = temp1;
+        row[numA] = box;
+        temp1 ="";
+//        System.out.println("box "+box);
+    }
+
+    public void RemoveNumFromBox(String box1, String num1, int numB) {
+        for (int f =0; f<box1.length(); f++){
+              if (!(box1.substring( f, f+1).equals(num1))){
+                   temp1+=box1.substring( f, f+1);
+                   System.out.println(temp1);
+              }
+          }
+        box1 = temp1;
+        box[numB] = box1;
+        temp1 ="";
+//        System.out.println("box "+box);
+        
+    }
+    
+    public void RemoveNumFromLine(String box, String num1, int numB) {
+        for (int f =0; f<box.length(); f++){
+              if (!(box.substring( f, f+1).equals(num1))){
+                   temp1+=box.substring( f, f+1);
+                   
+              }
+          }
+        box = temp1;
+        line[numB] = box;
+        temp1 ="";
+//        System.out.println("box "+box);
+        
     }
     
     public static void Reset(){
-    row1 = "123456789";
-    row2 = "123456789";
-    row3 = "123456789";
-    row4 = "123456789";
-    row5 = "123456789";
-    row6 = "123456789";
-    row7 = "123456789";
-    row8 = "123456789";
-    row9 = "123456789";
-    box1 = "123456789";
-    box2 = "123456789";
-    box3 = "123456789";
-    box4 = "123456789";
-    box5 = "123456789";
-    box6 = "123456789";
-    box7 = "123456789";
-    box8 = "123456789";
-    box9 = "123456789";
-    line1 = "123456789";
-    line2 = "123456789";
-    line3 = "123456789";
-    line4 = "123456789";
-    line5 = "123456789";
-    line6 = "123456789";
-    line7 = "123456789";
-    line8 = "123456789";
-    line9 = "123456789";
+    for (int index = 0; index < 9; index++) {
+            row[index] = "123456789";
+            box[index] = "123456789";
+            line[index] = "123456789";
+            System.out.println(index);
     }
+}
 }
